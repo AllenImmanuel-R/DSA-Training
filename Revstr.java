29e98be9dc32d9ee473fb9950406c15e446f6a5a
AllenImmanuel-R <immanuelallen23@gmail.com>
2025-06-19 16:13:01 +0530

    Add files via upload

---
+++ Revstr.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Revstr {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string to reverse:");
+        String str = sc.nextLine();
+        String reverse ="";
+        for(int i=str.length();i>0;i--)
+        {
+            reverse += str.charAt(i-1);
+        }
+        System.out.println("Original: " + str);
+        System.out.println("Reversed: " + reverse);
+        if (str.equals(reverse)) {
+            System.out.println("The string is a palindrome.");
+        } else {
+            System.out.println("The string is not a palindrome.");
+        }
+    }
+}
